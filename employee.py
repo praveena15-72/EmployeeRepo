a97910b8d5079bc334fcb89bbf332ee2f24d2a25
praveena prabhu <praveena.prabhu@tsworkz.com>
2022-03-29 10:32:32 +0530

    adding employe

---
+++ employee.py
@@ -0,0 +1,13 @@
+class employee:
+ def __init__(self, employeeId, employeeName, employeeLocation, employeeSalary):
+  self.employeeId = employeeId
+  self.employeeName = employeeName
+  self.employeeLocation = employeeLocation
+  self.employeeSalary = employeeSalary
+
+
+
+def getemployee(self):
+
+ print(str(self.employeeId) + " " + self.employeeName+ " " + self.employeeLocation+ " "
++ str(self.employeeSalary)+ " ")
---
+++ empmain.py
@@ -0,0 +1,18 @@
+from employeeclass import *
+
+
+
+employee1001 = employee("1001", "Ram", "Delhi", "15000")
+employee1002 = employee("1002", "Joshi", "Banglore", "20000")
+employee1003 = employee("1003", "Peter","Lucknow","25000")
+employee1004 = employee("1004", "Ashna","Chennai","25000")
+employee1005 = employee("1005", "Raghav","Kolkata","25000")
+
+
+
+
+employee1001.getemployee()
+employee1002.getemployee()
+employee1003.getemployee()
+employee1004.getemployee()
+employee1005.getemployee()
